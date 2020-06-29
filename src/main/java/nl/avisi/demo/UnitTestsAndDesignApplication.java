5c3ee2e4b7e2d22f3f22c21fad5cfa962ae5f86e
Jan Groothuijse <jan@groothuij.se>
2020-06-29 08:30:13 +0200

    Use start.spring.io to generate spring-boot project

---
+++ src/main/java/nl/avisi/demo/UnitTestsAndDesignApplication.java
@@ -0,0 +1,13 @@
+package nl.avisi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitTestsAndDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitTestsAndDesignApplication.class, args);
+	}
+
+}
